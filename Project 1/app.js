dd574bc98af11eb4f24a8d8764f0e80276842880
darkmuha <darkmuha10@gmail.com>
2022-12-05 21:52:00 +0100

    Fixed some issues with in Project 2 and 3. Restarted Project 1

--- Project 1/app.js
+++ Project 1/app.js
@@ -1,18 +1,25 @@
-const buttons = document.querySelectorAll('[data-carousel-button')
+// const buttons = document.querySelectorAll('[data-carousel-button')
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar-menu')
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const offset = button.dataset.carouselButton === 'next' ? 1 : -1
-    const slides = button
-      .closest('[data-carousel]')
-      .querySelector('[data-slides]')
+// buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+//     const slides = button
+//       .closest('[data-carousel]')
+//       .querySelector('[data-slides]')
 
-    const activeSlide = slides.querySelector('[data-active]')
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+//     const activeSlide = slides.querySelector('[data-active]')
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//     if (newIndex < 0) newIndex = slides.children.length - 1
+//     if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
+//     slides.children[newIndex].dataset.active = true
+//     delete activeSlide.dataset.active
+//   })
+// })
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
 })
---
+++ Project 3/app.txt
@@ -0,0 +1,46 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar-menu')
+const sections = document.querySelectorAll('section')
+const navigationList = document.querySelectorAll('.underLine')
+const herosButton = document.querySelector('.heroBtn')
+const dealsButton = document.querySelector('.dealsBtn')
+const staffsButton = document.querySelector('.staffsBtn')
+const aboutsButton = document.querySelector('.aboutsBtn')
+
+herosButton.addEventListener('click', () => {
+  herosButton.textContent = 'Book me! Button Clicked'
+})
+
+dealsButton.addEventListener('click', () => {
+  dealsButton.textContent = 'Book, Button Clicked'
+})
+
+staffsButton.addEventListener('click', () => {
+  staffsButton.textContent = 'Learn More, Button Clicked'
+})
+
+aboutsButton.addEventListener('click', () => {
+  aboutsButton.textContent = 'History, Button Clicked'
+})
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+})
+
+window.addEventListener('scroll', () => {
+  let curr = ''
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop
+    // const sectionHeight = section.clientHeight
+    if (pageYOffset > sectionTop) {
+      curr = section.getAttribute('id')
+    }
+  })
+  navigationList.forEach((li) => {
+    li.classList.remove('underLine')
+    if (li.classList.contains(curr)) {
+      li.classList.add('underLine')
+    }
+  })
+})
